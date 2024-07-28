90a651b2d7d333d66fda759fe035eb6c4e6bcebe
Ilyasahallal <159455114+Ilyasahallal@users.noreply.github.com>
2024-07-28 15:46:05 +0100

    Update reels_page.dart

--- lib/pages/reels_page.dart
+++ lib/pages/reels_page.dart
@@ -1,25 +1,55 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:ntrriniw_v0/components/my_app_bar.dart';
-import 'package:ntrriniw_v0/components/my_nav_bar.dart';
+import 'package:ntrriniw_v0/pages/screens/content_screen.dart';
+
 
 class ReelsPage extends StatefulWidget {
-  const ReelsPage({super.key});
-
   @override
-  State<ReelsPage> createState() => _ProfilePageState();
+  _ReelsPageState createState() => _ReelsPageState();
 }
 
-class _ProfilePageState extends State<ReelsPage> {
+class _ReelsPageState extends State<ReelsPage> {
+  final List<String> videos = [
+    'https://assets.mixkit.co/videos/52112/52112-720.mp4',
+    'https://assets.mixkit.co/videos/52089/52089-720.mp4'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        appBar: MyAppBar(),
-        body: Center(
-          child: Text(
-            "The reels page ",
-            style: TextStyle(fontSize: 24),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            children: [
+              Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return ContentScreen(
+                    src: videos[index],
+                  );
+                },
+                itemCount: videos.length,
+                scrollDirection: Axis.vertical,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Ntriniw Shorts',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Icon(Icons.camera_alt),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        bottomNavigationBar: MyNavBar(selectedIndex: 2));
+      ),
+    );
   }
 }
